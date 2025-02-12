1697729bf8c4910780d523f0e5103c64fc7c1da1
BALAJI S <balajisubash956@gmail.com>
2025-02-12 15:46:53 +0530

    bala

---
+++ reactapp/src/App.js
@@ -0,0 +1,22 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      
+        
+        <a
+          className="App-link"
+          href="https://github.com/BalajiCodings/bala-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        ><p>
+        Hello! Nanba
+      </p>
+        </a>
+      
+    </div>
+  );
+}
+
+export default App;
